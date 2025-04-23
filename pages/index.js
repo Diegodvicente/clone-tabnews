0f737488553efc75136f8ec012d81766512c26f2
Diegodvicente <111232216+Diegodvicente@users.noreply.github.com>
2025-04-23 22:57:34 +0000

    adiciona os arquivos de manifesto

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+function Home(){
+    return<h1>insanidade é continuar fazendo as mesma coisas e espera por resultados diferentes 
+        ass:Albert Einstein
+         </h1>
+   
+}
+
+export default Home;
